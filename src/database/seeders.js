69617f51da27b15e779469a2828046818084ea9d
thaysluna <thays.lima@academico.ifpb.edu.br>
2025-06-30 12:39:35 +0000

    .

--- src/database/seeders.js
+++ src/database/seeders.js
@@ -1,14 +1,14 @@
 import { resolve } from 'node:path';
 import { readFileSync } from 'node:fs';
-import books from '../models/books.js';
+import Books from '../models/books.js';
  
 async function up() {
   const file = resolve('src', 'database', 'seeders.json');
  
   const seed = JSON.parse(readFileSync(file));
  
-  for (const books of seed.investments) {
-    await books.create(books);
+  for (const books of seed.books) {
+    await Books.create(books);
   }
 }
  
