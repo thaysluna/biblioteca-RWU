98c02c8d8f8fc6bfb70f05ce36b4843eed01590c
May877 <mayara.viegas@academico.ifpb.edu.br>
2025-08-30 19:06:18 +0000

    ...

--- prisma/migrations/migration.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE livro (
-      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-      "title" VARCHAR(50) NOT NULL,
-      "author" VARCHAR(50) NOT NULL,
-      "description" VARCHAR(50) NOT NULL
-    );
